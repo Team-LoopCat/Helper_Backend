59dbd8621633f65f75c049900f4c23bf3e60fbad
오송주 <dhthdwn7920@gmail.com>
2024-09-19 20:16:29 +0900

    setting:: multi module setting

---
+++ helper/helper-persistence/src/main/java/org/example/HelperApplication.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelperApplication.class, args);
+    }
+}
