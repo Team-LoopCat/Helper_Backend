6d28198550095040368bd6db764464670f905fa7
오송주 <dhthdwn7920@gmail.com>
2024-10-08 14:39:52 +0900

    fix: 쓸모없는 어노테이션 삭제

--- helper/helper-persistence/src/main/java/org/example/persistence/student/repository/StudentJpaRepository.java
+++ helper/helper-persistence/src/main/java/org/example/persistence/student/repository/StudentJpaRepository.java
@@ -8,6 +8,5 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface StudentJpaRepository extends CrudRepository<StudentJpaEntity, String> {
-    @Query(value = "select * from student where studentId=:studentId", nativeQuery = true)
     Optional<StudentJpaEntity> findByStudentId(@Param("studentId") String studentId);
 }
