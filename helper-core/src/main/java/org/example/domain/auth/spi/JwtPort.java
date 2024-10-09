8ac636fd5b7465557f47d007bf5dc2878ed44659
오송주 <dhthdwn7920@gmail.com>
2024-10-09 23:14:29 +0900

    refactor: jwtPort 코드 사이에 여백 추가
    Co-authored-by: 강현욱 <93509214+zios0707@users.noreply.github.com>

--- helper-core/src/main/java/org/example/domain/auth/spi/JwtPort.java
+++ helper-core/src/main/java/org/example/domain/auth/spi/JwtPort.java
@@ -3,6 +3,8 @@ package org.example.domain.auth.spi;
 import org.example.domain.auth.model.Role;
 
 public interface JwtPort {
+
     String GenerateAccess(String userId, Role role);
+    
     String GenerateRefresh(String userId);
 }
