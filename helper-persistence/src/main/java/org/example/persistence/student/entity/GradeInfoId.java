6ff917b07120eac2213c84710840d5393f42af77
khyunwook <khyunwook0707@gmail.com>
2024-10-14 21:53:25 +0900

    style: 스타일 수정

--- helper-persistence/src/main/java/org/example/persistence/student/entity/GradeInfoId.java
+++ helper-persistence/src/main/java/org/example/persistence/student/entity/GradeInfoId.java
@@ -8,6 +8,8 @@ import java.io.Serializable;
 @EqualsAndHashCode
 @AllArgsConstructor
 public class GradeInfoId implements Serializable {
+
     private String grade;
+
     private String classroom;
 }
