9ade13a0f3bda282d5fec725c3bee690b59c48b9
오송주 <dhthdwn7920@gmail.com>
2024-10-17 20:00:53 +0900

    refactor: studentNumber method static으로 변경

--- helper-core/src/main/java/org/example/common/util/StudentNumberUtil.java
+++ helper-core/src/main/java/org/example/common/util/StudentNumberUtil.java
@@ -1,11 +1,8 @@
 package org.example.common.util;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class StudentNumberUtil {
 
-    public String combination(String grade, String classroom, String number) {
+    public static String combination(String grade, String classroom, String number) {
         return grade + classroom + String.format("%02d", Integer.parseInt(number));
     }
 }
