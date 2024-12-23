84314d4d6d061e1f9f43849b42cfafffcf8719d3
오송주 <dhthdwn7920@gmail.com>
2024-12-23 18:34:10 +0900

    feat: getStudentInfo webAdapter + ResponseDto 제작

--- helper-core/src/main/java/org/example/presentation/StudentWebAdapter.java
+++ helper-core/src/main/java/org/example/presentation/StudentWebAdapter.java
@@ -2,6 +2,8 @@ package org.example.presentation;
 
 import lombok.RequiredArgsConstructor;
 import org.example.domain.student.dto.request.SignupRequestDto;
+import org.example.domain.student.dto.response.GetMyInfoResponseDto;
+import org.example.domain.student.usecase.GetMyInfoUseCase;
 import org.example.domain.student.usecase.SignupUseCase;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,10 +13,17 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/student")
 public class StudentWebAdapter {
     private final SignupUseCase signupUseCase;
+    private final GetMyInfoUseCase getMyInfoUseCase;
 
     @ResponseStatus(HttpStatus.OK)
     @PostMapping("/signup")
     public void signup (@RequestBody SignupRequestDto request) {
         signupUseCase.execute(request);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/my")
+    public GetMyInfoResponseDto getMyInfo () {
+        return getMyInfoUseCase.execute();
     }
 }
