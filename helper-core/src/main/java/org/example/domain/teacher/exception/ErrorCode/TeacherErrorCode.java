ae88429a082da8e5f5809cdfa63bd57717f513da
khyunwook <khyunwook0707@gmail.com>
2024-12-24 16:17:27 +0900

    feat: TeacherNotFound 예외 추가

--- helper-core/src/main/java/org/example/domain/teacher/exception/ErrorCode/TeacherErrorCode.java
+++ helper-core/src/main/java/org/example/domain/teacher/exception/ErrorCode/TeacherErrorCode.java
@@ -5,6 +5,7 @@ import org.example.common.exception.GlobalErrorCode;
 
 @RequiredArgsConstructor
 public enum TeacherErrorCode implements GlobalErrorCode {
+    TEACHER_NOT_FOUND(404, "존재하지 않는 선생님입니다"),
     ID_ALREADY_EXIST(409, "이미 존재하는 아이디입니다.");
 
     private final int errorStatus;
