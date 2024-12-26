2329d60f56dc047646195bc369d0791b789ef44d
khyunwook <khyunwook0707@gmail.com>
2024-12-26 15:19:52 +0900

    chore: RequestBody 추가

--- helper-core/src/main/java/org/example/presentation/TeacherWebAdapter.java
+++ helper-core/src/main/java/org/example/presentation/TeacherWebAdapter.java
@@ -5,6 +5,7 @@ import org.example.domain.teacher.dto.request.CheckIdExistRequestDto;
 import org.example.domain.teacher.usecase.CheckIdExistUseCase;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ public class TeacherWebAdapter {
 
     @ResponseStatus(HttpStatus.OK)
     @PostMapping("/check")
-    public void checkId(CheckIdExistRequestDto checkIdRequest) {
+    public void checkId(@RequestBody CheckIdExistRequestDto checkIdRequest) {
         checkIdExistUseCase.execute(checkIdRequest);
     }
 }
