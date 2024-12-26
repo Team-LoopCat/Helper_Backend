bd6d465b549141b038a631559ad05e0f0c821d2f
khyunwook <khyunwook0707@gmail.com>
2024-12-26 15:58:08 +0900

    feat: currentUser 메서드 추가

--- helper-core/src/main/java/org/example/common/service/SecurityService.java
+++ helper-core/src/main/java/org/example/common/service/SecurityService.java
@@ -1,8 +1,12 @@
 package org.example.common.service;
 
+import org.example.domain.auth.model.User;
+
 public interface SecurityService {
 
     String getCurrentUserId();
+
+    User getCurrentUser();
 
     void checkPasswordMatches(String rawPassword, String encodedPassword);
 }
