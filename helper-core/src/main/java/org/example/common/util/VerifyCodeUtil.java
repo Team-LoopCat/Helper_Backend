d25a4ddb1cbbff6098aa30228c124d061b393080
오송주 <dhthdwn7920@gmail.com>
2024-12-27 17:00:33 +0900

    feat: 코드를 생성해주는 Util 추가

---
+++ helper-core/src/main/java/org/example/common/util/VerifyCodeUtil.java
@@ -0,0 +1,18 @@
+package org.example.common.util;
+
+import java.util.Random;
+
+public class VerifyCodeUtil {
+
+    public static String createRandomCode() {
+        Random random = new Random();
+        StringBuilder randomNumber = new StringBuilder();
+
+        for (int i = 0; i < 5; i++) {
+            int digit = random.nextInt(10);
+            randomNumber.append(digit);
+        }
+
+        return randomNumber.toString();
+    }
+}
