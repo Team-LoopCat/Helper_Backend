c0447d458ff430e55cec3c09c59493103a95f07a
오송주 <dhthdwn7920@gmail.com>
2024-12-27 16:42:28 +0900

    feat: send email 관련 Adapter 코드 추가

--- helper-core/src/main/java/org/example/presentation/StudentWebAdapter.java
+++ helper-core/src/main/java/org/example/presentation/StudentWebAdapter.java
@@ -1,10 +1,12 @@
 package org.example.presentation;
 
 import lombok.RequiredArgsConstructor;
+import org.example.domain.student.dto.request.SendCodeRequestDto;
 import org.example.domain.student.dto.request.SignupRequestDto;
 import org.example.domain.student.dto.response.GetMyInfoResponseDto;
 import org.example.domain.student.usecase.GetMyInfoUseCase;
 import org.example.domain.student.usecase.DeleteStudentUseCase;
+import org.example.domain.student.usecase.SendCodeUseCase;
 import org.example.domain.student.usecase.SignupUseCase;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,7 @@ public class StudentWebAdapter {
     private final SignupUseCase signupUseCase;
     private final GetMyInfoUseCase getMyInfoUseCase;
     private final DeleteStudentUseCase deleteStudentUseCase;
+    private final SendCodeUseCase sendCodeUseCase;
 
     @ResponseStatus(HttpStatus.OK)
     @PostMapping("/signup")
@@ -27,11 +30,17 @@ public class StudentWebAdapter {
     @GetMapping("/my")
     public GetMyInfoResponseDto getMyInfo () {
         return getMyInfoUseCase.execute();
-
+    }
   
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping
     public void deleteStudent () {
         deleteStudentUseCase.execute();
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/email/send")
+    public void sendCode (@RequestBody SendCodeRequestDto request) {
+        sendCodeUseCase.execute(request);
     }
 }
