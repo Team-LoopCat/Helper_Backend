eb6434e01b387f3bad4bad3e3b5f907c9fc5b4d6
오송주 <dhthdwn7920@gmail.com>
2024-12-27 16:43:26 +0900

    feat: 이메일 보내는 비지니스 로직 추가

--- helper-core/src/main/java/org/example/domain/student/exception/errorCode/StudentErrorCode.java
+++ helper-core/src/main/java/org/example/domain/student/exception/errorCode/StudentErrorCode.java
@@ -9,7 +9,8 @@ public enum StudentErrorCode implements GlobalErrorCode {
     STUDENT_NOT_FOUND(404, "존재하지 않는 학생입니다"),
     EMAIL_ALREADY_EXISTS(409, "이미 가입된 이메일입니다"),
     USER_ID_ALREADY_EXISTS(409, "이미 가입된 유저 아이디입니다"),
-    STUDENT_NUMBER_ALREADY_EXISTS(409, "이미 가입된 학번입니다");
+    STUDENT_NUMBER_ALREADY_EXISTS(409, "이미 가입된 학번입니다"),
+    EMAIL_SENDING_EXCEPTION(500, "이메일을 보내는 과정에서 오류가 생겼습니다");
 
     private final int errorStatus;
     private final String errorMessage;
