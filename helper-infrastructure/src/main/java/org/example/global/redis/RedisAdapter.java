52460be462342d531b379356f462ea3da07ec0f5
오송주 <dhthdwn7920@gmail.com>
2024-12-27 16:41:08 +0900

    setting: Redis 관련 설정

---
+++ helper-infrastructure/src/main/java/org/example/global/redis/RedisAdapter.java
@@ -0,0 +1,38 @@
+package org.example.global.redis;
+
+import lombok.RequiredArgsConstructor;
+import org.example.common.service.RedisService;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+@RequiredArgsConstructor
+public class RedisAdapter implements RedisService {
+    private final StringRedisTemplate redis;
+
+    @Override
+    public void setData(String key, String data, long duration) {
+        ValueOperations<String, String> valueOperations = redis.opsForValue();
+        Duration expireDuration = Duration.ofSeconds(duration);
+        valueOperations.set(key, data, expireDuration);
+    }
+
+    @Override
+    public boolean checkDataExists(String key) {
+        return Boolean.TRUE.equals(redis.hasKey(key));
+    }
+
+    @Override
+    public String getData(String key) {
+        ValueOperations<String, String> valueOperations = redis.opsForValue();
+        return valueOperations.get(key);
+    }
+
+    @Override
+    public void deleteData(String key) {
+        redis.delete(key);
+    }
+}
