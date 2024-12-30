ef79fa284b920611aefe625daa7a1fb128fceee6
khyunwook <khyunwook0707@gmail.com>
2024-12-30 15:12:24 +0900

    feat: WebAdapter

--- helper-core/src/main/java/org/example/presentation/StudyWebAdapter.java
+++ helper-core/src/main/java/org/example/presentation/StudyWebAdapter.java
@@ -1,10 +1,13 @@
 package org.example.presentation;
 
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.example.domain.study.dto.request.PostStudyRequestDto;
 import org.example.domain.study.dto.response.PostStudyResponseDto;
 import org.example.domain.study.usecase.PostStudyUseCase;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +24,11 @@ public class StudyWebAdapter {
     @PostMapping
     public PostStudyResponseDto postStudy(@RequestBody PostStudyRequestDto postStudyRequest) {
         return postStudyUseCase.execute(postStudyRequest);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{study_id}")
+    public void deleteStudy(@PathVariable("study_id") UUID studyId) {
+
     }
 }
