7fbdfd1fb40993a445d10eca20cf4e6976e53c8c
khyunwook <khyunwook0707@gmail.com>
2024-12-30 15:13:02 +0900

    feat: PersistenceAdapter, Port

--- helper-core/src/main/java/org/example/domain/study/spi/QueryStudyPort.java
+++ helper-core/src/main/java/org/example/domain/study/spi/QueryStudyPort.java
@@ -1,8 +1,14 @@
 package org.example.domain.study.spi;
 
+import java.util.Optional;
+import java.util.UUID;
 import org.example.domain.study.model.Study;
 
 public interface QueryStudyPort {
 
+    Optional<Study> findByStudyId(UUID studyId);
+
     Study saveStudy(Study study);
+
+    void deleteStudyByStudyId(UUID studyId);
 }
--- helper-persistence/src/main/java/org/example/persistence/study/StudyPersistenceAdapter.java
+++ helper-persistence/src/main/java/org/example/persistence/study/StudyPersistenceAdapter.java
@@ -1,6 +1,7 @@
 package org.example.persistence.study;
 
 import java.util.Optional;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.example.domain.study.model.Study;
 import org.example.domain.study.spi.QueryStudyPort;
@@ -15,11 +16,23 @@ public class StudyPersistenceAdapter implements QueryStudyPort {
     private final StudyMapper studyMapper;
 
     @Override
+    public Optional<Study> findByStudyId(UUID studyId) {
+        return studyMapper.toDomain(
+                studyJpaRepository.findById(studyId)
+        );
+    }
+
+    @Override
     public Study saveStudy(Study study) {
         return studyMapper.toDomain(
                 Optional.of(studyJpaRepository.save(
                         studyMapper.toEntity(study)
                 ))
         ).get();
+    }
+
+    @Override
+    public void deleteStudyByStudyId(UUID studyId) {
+        studyJpaRepository.deleteById(studyId);
     }
 }
