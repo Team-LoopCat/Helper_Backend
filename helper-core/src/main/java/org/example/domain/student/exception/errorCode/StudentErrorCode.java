6ebf4dc9d6f3a65ab947244c49fd02bdf1f9084c
오송주 <dhthdwn7920@gmail.com>
2024-12-31 14:35:55 +0900

    fix: 이메일 관련 에러코드 statusCode와 설명 수정

--- helper-core/src/main/java/org/example/domain/student/exception/errorCode/StudentErrorCode.java
+++ helper-core/src/main/java/org/example/domain/student/exception/errorCode/StudentErrorCode.java
@@ -5,12 +5,12 @@ import org.example.common.exception.GlobalErrorCode;
 
 @RequiredArgsConstructor
 public enum StudentErrorCode implements GlobalErrorCode {
+    EMAIL_SENDING_EXCEPTION(400, "코드를 수신받는 이메일이 존재하지 않거나 비활성된 상태 같습니다."),
     GRADE_NOT_EXISTS(404, "존재하지 않는 학년/반입니다"),
     STUDENT_NOT_FOUND(404, "존재하지 않는 학생입니다"),
     EMAIL_ALREADY_EXISTS(409, "이미 가입된 이메일입니다"),
     USER_ID_ALREADY_EXISTS(409, "이미 가입된 유저 아이디입니다"),
-    STUDENT_NUMBER_ALREADY_EXISTS(409, "이미 가입된 학번입니다"),
-    EMAIL_SENDING_EXCEPTION(500, "이메일을 보내는 과정에서 오류가 생겼습니다");
+    STUDENT_NUMBER_ALREADY_EXISTS(409, "이미 가입된 학번입니다");
 
     private final int errorStatus;
     private final String errorMessage;
