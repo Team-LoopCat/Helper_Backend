a31bcb1eec8bd7e176c81ec16b2ee10bee6a1486
오송주 <dhthdwn7920@gmail.com>
2025-01-03 17:11:26 +0900

    refector: jwtServiceInterface 공백 추가

--- helper-core/src/main/java/org/example/common/service/JwtService.java
+++ helper-core/src/main/java/org/example/common/service/JwtService.java
@@ -4,5 +4,6 @@ import org.example.domain.auth.dto.response.LoginResponseDto;
 import org.example.domain.auth.model.Role;
 
 public interface JwtService {
+
     LoginResponseDto makeJwtTokens(String userId, Role role);
 }
