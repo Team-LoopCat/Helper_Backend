c56d415a56a21bf56f8a1259370c30e8cd3f930f
오송주 <dhthdwn7920@gmail.com>
2025-01-06 16:07:47 +0900

    fix: conflict 해결과정에서 일부 누락된 import와 필요없는 import 제거

--- helper-core/src/main/java/org/example/presentation/StudentWebAdapter.java
+++ helper-core/src/main/java/org/example/presentation/StudentWebAdapter.java
@@ -4,12 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.example.domain.student.dto.request.ChangePasswordRequestDto;
 import org.example.domain.student.dto.request.SendCodeRequestDto;
 import org.example.domain.student.dto.request.SignupRequestDto;
+import org.example.domain.student.dto.request.UpdateStudentRequestDto;
 import org.example.domain.student.dto.request.VerifyCodeRequestDto;
 import org.example.domain.student.dto.response.GetMyInfoResponseDto;
 import org.example.domain.student.usecase.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
