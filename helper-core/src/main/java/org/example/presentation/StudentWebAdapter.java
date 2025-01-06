dcd4fd2cbcbe1aef4c3c0f3f6ae8340899bc1cd2
오송주 <dhthdwn7920@gmail.com>
2025-01-06 16:09:56 +0900

    fix: rebase 과정에서 생긴 일부 중복코드 제거

--- helper-core/src/main/java/org/example/presentation/StudentWebAdapter.java
+++ helper-core/src/main/java/org/example/presentation/StudentWebAdapter.java
@@ -22,7 +22,6 @@ public class StudentWebAdapter {
     private final VerifyCodeUseCase verifyCodeUseCase;
     private final UpdateUserUseCase updateUserUseCase;
     private final ChangePasswordUseCase changePasswordUseCase;
-    private final ChangePasswordUseCase changePasswordUseCase;
 
     @ResponseStatus(HttpStatus.OK)
     @PostMapping("/signup")
@@ -58,12 +57,6 @@ public class StudentWebAdapter {
     @PatchMapping("/profile")
     public void updateUser (@RequestBody UpdateStudentRequestDto request) {
         updateUserUseCase.execute(request);
-    }
-
-    @ResponseStatus(HttpStatus.OK)
-    @PatchMapping("/password")
-    public void changePassword(@RequestBody ChangePasswordRequestDto request) {
-        changePasswordUseCase.execute(request);
     }
 
     @ResponseStatus(HttpStatus.OK)
