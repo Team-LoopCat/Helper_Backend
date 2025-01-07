da9349e637fb6c0811d72ad5c0ede9ed530dfefb
오송주 <dhthdwn7920@gmail.com>
2025-01-07 16:49:23 +0900

    feat: 파일 업로드 기능 제작

---
+++ helper-core/src/main/java/org/example/domain/util/usecase/UploadFileUseCase.java
@@ -0,0 +1,19 @@
+package org.example.domain.util.usecase;
+
+import lombok.RequiredArgsConstructor;
+import org.example.common.service.FileService;
+import org.example.domain.util.dto.response.UploadFileResponseDto;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UploadFileUseCase {
+    private final FileService fileService;
+
+    public UploadFileResponseDto execute(MultipartFile file) {
+        return new UploadFileResponseDto(fileService.uploadFile(file));
+    }
+}
