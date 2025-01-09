1cf72744063c42a7915e3f86de3fa3b8946a12d4
khyunwook <khyunwook0707@gmail.com>
2025-01-09 16:05:24 +0900

    feat: UseCase

---
+++ helper-core/src/main/java/org/example/domain/study/usecase/StudyBanStudentsUseCase.java
@@ -0,0 +1,48 @@
+package org.example.domain.study.usecase;
+
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.example.common.service.SecurityService;
+import org.example.domain.student.model.Student;
+import org.example.domain.student.service.GetStudentService;
+import org.example.domain.study.model.Member;
+import org.example.domain.study.model.Study;
+import org.example.domain.study.service.CheckMemberService;
+import org.example.domain.study.service.CheckStudyService;
+import org.example.domain.study.service.CommandMemberService;
+import org.example.domain.study.service.GetMemberService;
+import org.example.domain.study.service.GetStudyService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class StudyBanStudentsUseCase {
+    private final GetStudyService getStudyService;
+    private final GetStudentService getStudentService;
+    private final GetMemberService getMemberService;
+    private final CommandMemberService commandMemberService;
+    private final CheckStudyService checkStudyService;
+    private final CheckMemberService checkMemberService;
+    private final SecurityService securityService;
+
+    public void execute(UUID studyId, String studentId) {
+        Study currentStudy = getStudyService.getStudyById(studyId);
+        Student banStudent = getStudentService.getStudentByStudentId(studentId);
+
+        // member 존재 여부 확인
+        getMemberService.getByStudyAndStudent(currentStudy, banStudent);
+
+        Student currentStudent = getStudentService.getStudentByUserId(securityService.getCurrentUser());
+        checkStudyService.checkStudyIsOwn(currentStudy, currentStudent);
+
+        commandMemberService.saveMember(
+                Member.builder()
+                        .studyId(studyId)
+                        .studentId(studentId)
+                        .isBanned(true)
+                        .build()
+        );
+    }
+}
