08e8a2cd143dd9487eaf7df86a90bf8733c228aa
khyunwook <khyunwook0707@gmail.com>
2025-01-09 15:57:45 +0900

    fix: checkStudyIsOwn 잘못된 로직 수정

--- helper-core/src/main/java/org/example/domain/study/service/CheckStudyService.java
+++ helper-core/src/main/java/org/example/domain/study/service/CheckStudyService.java
@@ -1,9 +1,9 @@
 package org.example.domain.study.service;
 
-import org.example.domain.auth.model.User;
+import org.example.domain.student.model.Student;
 import org.example.domain.study.model.Study;
 
 public interface CheckStudyService {
 
-    void checkStudyIsOwn(Study study, User user);
+    void checkStudyIsOwn(Study study, Student student);
 }
