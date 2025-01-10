c33be4c3734eb1da37f51d502665832c25aae2c3
오송주 <dhthdwn7920@gmail.com>
2025-01-10 16:48:37 +0900

    feat: deleteAllByPost 메소드 개발

--- helper-core/src/main/java/org/example/domain/file/service/impl/CommandFileServiceImpl.java
+++ helper-core/src/main/java/org/example/domain/file/service/impl/CommandFileServiceImpl.java
@@ -2,17 +2,23 @@ package org.example.domain.file.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.example.domain.file.model.File;
-import org.example.domain.file.service.CommendFileService;
+import org.example.domain.file.service.CommandFileService;
 import org.example.domain.file.spi.QueryFilePort;
+import org.example.domain.post.model.Post;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CommendFileServiceImpl implements CommendFileService {
+public class CommandFileServiceImpl implements CommandFileService {
     private final QueryFilePort queryFilePort;
 
     @Override
     public void saveFile(File file) {
         queryFilePort.saveFile(file);
+    }
+
+    @Override
+    public void deleteAllByPost(Post post) {
+        queryFilePort.deleteAllByPost(post);
     }
 }
