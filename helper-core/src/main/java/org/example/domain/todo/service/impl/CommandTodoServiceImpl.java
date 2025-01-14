42cb548f9d39af364050909af74dccec29715821
khyunwook <khyunwook0707@gmail.com>
2025-01-14 15:19:43 +0900

    feat: 누락 코드 추가

--- helper-core/src/main/java/org/example/domain/todo/service/impl/CommandTodoServiceImpl.java
+++ helper-core/src/main/java/org/example/domain/todo/service/impl/CommandTodoServiceImpl.java
@@ -3,14 +3,16 @@ package org.example.domain.todo.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.example.domain.todo.model.Todo;
 import org.example.domain.todo.service.CommandTodoService;
+import org.example.domain.todo.spi.TodoQueryPort;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CommandTodoServiceImpl implements CommandTodoService {
+    private final TodoQueryPort todoQueryPort;
 
     @Override
     public Todo saveTodo(Todo todo) {
-        return null;
+        return todoQueryPort.saveTodo(todo);
     }
 }
