a6e46a6d11983aa8e6e6dc9c5448e635d9738114
오송주 <dhthdwn7920@gmail.com>
2025-01-14 14:09:53 +0900

    setting: 400 에러를 위한 build gradle 수정 + exception handler 설정 추가

--- helper-infrastructure/src/main/java/org/example/global/exception/GeneralExceptionCode.java
+++ helper-infrastructure/src/main/java/org/example/global/exception/GeneralExceptionCode.java
@@ -5,6 +5,7 @@ import org.example.common.exception.GlobalErrorCode;
 
 @RequiredArgsConstructor
 public enum GeneralExceptionCode implements GlobalErrorCode {
+    BAD_REQUEST(400, "유효하지 않은 요청입니다"),
     INTERNAL_SERVER_ERROR(500, "서버 오류입니다");
 
     private final int errorStatus;
