3b010971984b8f35763c05df68a931cdd290d7cc
오송주 <dhthdwn7920@gmail.com>
2025-01-14 16:14:01 +0900

    fix: 잘못 오타난 부분 + 누락된 코드들 추가

--- helper-core/src/main/java/org/example/presentation/CommentWebAdapter.java
+++ helper-core/src/main/java/org/example/presentation/CommentWebAdapter.java
@@ -4,16 +4,18 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.domain.comment.dto.request.CreateCommentRequestDto;
 import org.example.domain.comment.usecase.CreateCommentUseCase;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/post")
+@RequestMapping("/comment")
 public class CommentWebAdapter {
     private final CreateCommentUseCase createCommentUseCase;
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/create/{postId}")
     private void createComment(@Valid @PathVariable UUID postId, @Valid @RequestBody CreateCommentRequestDto request) {
         createCommentUseCase.execute(postId, request);
