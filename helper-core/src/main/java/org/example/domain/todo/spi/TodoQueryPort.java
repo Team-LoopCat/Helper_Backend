6bb99ef750f74c47eafc4228a44776c212d7414c
khyunwook <khyunwook0707@gmail.com>
2025-01-15 16:45:30 +0900

    feat: PersistenceAdapter, Port

--- helper-core/src/main/java/org/example/domain/todo/spi/TodoQueryPort.java
+++ helper-core/src/main/java/org/example/domain/todo/spi/TodoQueryPort.java
@@ -1,8 +1,12 @@
 package org.example.domain.todo.spi;
 
+import java.util.Optional;
+import java.util.UUID;
 import org.example.domain.todo.model.Todo;
 
 public interface TodoQueryPort {
 
     Todo saveTodo(Todo todo);
+
+    Optional<Todo> findTodoById(UUID id);
 }
--- helper-persistence/src/main/java/org/example/persistence/todo/repository/TodoJpaRepository.java
+++ helper-persistence/src/main/java/org/example/persistence/todo/repository/TodoJpaRepository.java
@@ -1,10 +1,11 @@
 package org.example.persistence.todo.repository;
 
+import java.util.UUID;
 import org.example.persistence.todo.entity.TodoJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TodoJpaRepository extends JpaRepository<TodoJpaEntity, Long> {
+public interface TodoJpaRepository extends JpaRepository<TodoJpaEntity, UUID> {
 
 }
