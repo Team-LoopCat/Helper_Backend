b3c31988254ad0cee8b801e5d3457c8da99cd9a8
오송주 <dhthdwn7920@gmail.com>
2025-01-17 16:37:11 +0900

    feat: getCommentService 개발

--- helper-persistence/src/main/java/org/example/persistence/comment/repository/CommentJpaRepository.java
+++ helper-persistence/src/main/java/org/example/persistence/comment/repository/CommentJpaRepository.java
@@ -1,11 +1,21 @@
 package org.example.persistence.comment.repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import org.example.domain.comment.spi.vo.CommentDataVO;
 import org.example.persistence.comment.entity.CommentJpaEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface CommentJpaRepository extends CrudRepository<CommentJpaEntity, UUID> {
 
     Optional<CommentJpaEntity> findByCommentId(UUID commentId);
+
+    @Query(value = "SELECT c.commentId, c.content, c.createdAt, s.studentId, s.nickname " +
+           "FROM student s INNER JOIN comment c ON s.studentId = c.student.studentId" +
+           " WHERE c.post.postId = :postId")
+    List<CommentDataVO> findAllWithWriterByPostId(@Param("postId") UUID postId);
 }
