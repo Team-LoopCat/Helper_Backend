661deb0645b841e27b125e5aa11b3c3f5369fdb3
오송주 <dhthdwn7920@gmail.com>
2025-01-22 15:07:06 +0900

    feat: usecase & webadapter

---
+++ helper-core/src/main/java/org/example/presentation/ReplyWebAdapter.java
@@ -0,0 +1,22 @@
+package org.example.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.example.domain.comment.dto.request.CreateReplyRequestDto;
+import org.example.domain.comment.usecase.CreateReplyUseCase;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/reply")
+public class ReplyWebAdapter {
+    private final CreateReplyUseCase createReplyUseCase;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/create/{commentId}")
+    public void createPost(@RequestBody CreateReplyRequestDto request, @PathVariable("commentId") UUID commentId) {
+        createReplyUseCase.execute(request, commentId);
+    }
+}
