23a9ff07bbad4205dc61b0ec3dd7bec7794b4a67
khyunwook <khyunwook0707@gmail.com>
2025-02-04 16:59:07 +0900

    refactor: 쿼리 로직

--- helper-core/src/main/java/org/example/domain/exam/spi/QueryExamPort.java
+++ helper-core/src/main/java/org/example/domain/exam/spi/QueryExamPort.java
@@ -2,10 +2,13 @@ package org.example.domain.exam.spi;
 
 import java.util.List;
 import org.example.domain.exam.model.Exam;
+import org.example.domain.exam.model.ExamData;
 
 public interface QueryExamPort {
 
     boolean existsAnyExam();
 
-    List<Exam> saveAll(List<Exam> exams);
+    Exam saveExam(Exam exam);
+
+    List<ExamData> saveAllExamData(List<ExamData> examData);
 }
