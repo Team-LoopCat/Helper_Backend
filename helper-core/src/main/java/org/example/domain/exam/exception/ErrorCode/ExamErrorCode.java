cfe7837645ce89a4c98d738322c508d416c08877
khyunwook <khyunwook0707@gmail.com>
2025-02-19 17:11:30 +0900

    feat: ErrorCode 추가

--- helper-core/src/main/java/org/example/domain/exam/exception/ErrorCode/ExamErrorCode.java
+++ helper-core/src/main/java/org/example/domain/exam/exception/ErrorCode/ExamErrorCode.java
@@ -5,7 +5,8 @@ import org.example.common.exception.GlobalErrorCode;
 
 @RequiredArgsConstructor
 public enum ExamErrorCode implements GlobalErrorCode {
-    ALREADY_STARTED(409, "이미 시험이 시작되었습니다");
+    ALREADY_STARTED(409, "이미 시험이 시작되었습니다"),
+    DUPLICATE_DATE_OR_PERIOD(409, "이미 존재하는 시험 날짜 및 교시입니다");
 
     private final int errorStatus;
     private final String errorMessage;
