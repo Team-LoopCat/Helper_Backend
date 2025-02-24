9a6030b660f6fa242e6c004a09a858ac104da979
khyunwook <khyunwook0707@gmail.com>
2025-02-24 17:15:53 +0900

    feat: PersistenceAdapter, Port

--- helper-core/src/main/java/org/example/domain/exam/spi/QueryExamDataPort.java
+++ helper-core/src/main/java/org/example/domain/exam/spi/QueryExamDataPort.java
@@ -3,6 +3,7 @@ package org.example.domain.exam.spi;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 import org.example.domain.exam.model.ExamData;
 
 public interface QueryExamDataPort {
@@ -12,6 +13,8 @@ public interface QueryExamDataPort {
     ExamData saveExamData(ExamData examData);
 
     Optional<ExamData> queryFirstExamDataOrderByDateDesc();
+
+    Optional<ExamData> queryExamDataById(UUID examDataId);
 
     Boolean existsExamDataByDateAndPeriod(LocalDate date, Integer period);
 }
