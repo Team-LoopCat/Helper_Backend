b8d0921ee72c2b6d26491a0988a66288a858829f
khyunwook <khyunwook0707@gmail.com>
2025-02-24 17:15:31 +0900

    feat: UseCase, Service

--- helper-core/src/main/java/org/example/domain/exam/service/impl/GetExamDataServiceImpl.java
+++ helper-core/src/main/java/org/example/domain/exam/service/impl/GetExamDataServiceImpl.java
@@ -1,10 +1,21 @@
 package org.example.domain.exam.service.impl;
 
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.example.domain.exam.exception.ExamDataNotFoundException;
+import org.example.domain.exam.model.ExamData;
 import org.example.domain.exam.service.GetExamDataService;
+import org.example.domain.exam.spi.QueryExamDataPort;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class GetExamDataServiceImpl implements GetExamDataService {
+    private final QueryExamDataPort queryExamDataPort;
+
+    public ExamData getExamDataById(UUID examDataId) {
+        return queryExamDataPort.queryExamDataById(examDataId).orElseThrow(
+                () -> ExamDataNotFoundException.EXCEPTION
+        );
+    }
 }
---
+++ helper-core/src/main/java/org/example/domain/exam/usecase/UpdateExamDataUseCase.java
@@ -0,0 +1,41 @@
+package org.example.domain.exam.usecase;
+
+import jakarta.validation.Valid;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.example.domain.exam.dto.request.UpdateExamDataRequestDto;
+import org.example.domain.exam.model.ExamData;
+import org.example.domain.exam.service.CommandExamDataService;
+import org.example.domain.exam.service.GetExamDataService;
+import org.example.domain.subject.model.Subject;
+import org.example.domain.subject.service.GetSubjectService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UpdateExamDataUseCase {
+    private final GetExamDataService getExamDataService;
+    private final GetSubjectService getSubjectService;
+    private final CommandExamDataService commandExamDataService;
+
+    public void execute(UUID examDataId, @Valid @RequestBody UpdateExamDataRequestDto request) {
+        ExamData currentExamData = getExamDataService.getExamDataById(examDataId);
+        Subject subject = getSubjectService.getSubjectById(request.subjectId());
+
+        commandExamDataService.saveExamData(
+                ExamData.builder()
+                        .examDataId(currentExamData.getExamDataId())
+                        .examId(currentExamData.getExamId())
+                        .subjectId(subject.getSubjectId())
+                        .date(currentExamData.getDate())
+                        .period(currentExamData.getPeriod())
+                        .problems(currentExamData.getProblems())
+                        .percent(currentExamData.getPercent())
+                        .content(currentExamData.getContent())
+                        .build()
+        );
+    }
+}
