eb8211e7d47c38b7db9509f32c0f047d4ffd9a49
khyunwook <khyunwook0707@gmail.com>
2025-02-24 17:14:01 +0900

    feat: ErrorCode 추가

--- helper-core/src/main/java/org/example/domain/exam/exception/ErrorCode/ExamErrorCode.java
+++ helper-core/src/main/java/org/example/domain/exam/exception/ErrorCode/ExamErrorCode.java
@@ -5,6 +5,7 @@ import org.example.common.exception.GlobalErrorCode;
 
 @RequiredArgsConstructor
 public enum ExamErrorCode implements GlobalErrorCode {
+    EXAM_DATA_NOT_FOUND(404, "존재하지 않는 시험 정보 입니다"),
     ALREADY_STARTED(409, "이미 시험이 시작되었습니다"),
     DUPLICATE_DATE_AND_PERIOD(409, "이미 존재하는 시험 날짜 및 교시입니다");
 
