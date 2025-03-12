2ecad5b6de18741b25846f1b751efd01f1ef8006
khyunwook <khyunwook0707@gmail.com>
2025-03-12 19:13:28 +0900

    feat: UseCase, Service

--- helper-core/src/main/java/org/example/domain/exam/service/GetExamDataService.java
+++ helper-core/src/main/java/org/example/domain/exam/service/GetExamDataService.java
@@ -1,9 +1,13 @@
 package org.example.domain.exam.service;
 
+import java.util.List;
 import java.util.UUID;
 import org.example.domain.exam.model.ExamData;
+import org.example.domain.exam.spi.vo.SimpleExamDataVO;
 
 public interface GetExamDataService {
 
     ExamData getExamDataById(UUID examDataId);
+
+    List<SimpleExamDataVO> getAllExamDataByExamDataId(UUID examDataId);
 }
--- helper-core/src/main/java/org/example/domain/exam/service/impl/GetExamDataServiceImpl.java
+++ helper-core/src/main/java/org/example/domain/exam/service/impl/GetExamDataServiceImpl.java
@@ -1,11 +1,13 @@
 package org.example.domain.exam.service.impl;
 
+import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.example.domain.exam.exception.ExamDataNotFoundException;
 import org.example.domain.exam.model.ExamData;
 import org.example.domain.exam.service.GetExamDataService;
 import org.example.domain.exam.spi.QueryExamDataPort;
+import org.example.domain.exam.spi.vo.SimpleExamDataVO;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,5 +19,10 @@ public class GetExamDataServiceImpl implements GetExamDataService {
         return queryExamDataPort.queryExamDataById(examDataId).orElseThrow(
                 () -> ExamDataNotFoundException.EXCEPTION
         );
+    }
+
+    @Override
+    public List<SimpleExamDataVO> getAllExamDataByExamDataId(UUID examDataId) {
+        return queryExamDataPort.queryAllExamDataByExamDataId(examDataId);
     }
 }
